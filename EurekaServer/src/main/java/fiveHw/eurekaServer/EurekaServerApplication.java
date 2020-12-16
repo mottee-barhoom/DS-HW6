9616c1ac3d93045a0cc80a4c8a1b91cf96b1c08c
mottee_barhoom <65988924+mottee-barhoom@users.noreply.github.com>
2020-12-16 08:30:05 +0200

    Add files via upload

---
+++ EurekaServer/src/main/java/fiveHw/eurekaServer/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package fiveHw.eurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+/**
+ *
+ * @author M M BARHOOM
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
---
+++ EurekaServer/src/test/java/fiveHw/eurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package fiveHw.eurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+/**
+ *
+ * @author M M BARHOOM
+ */
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
